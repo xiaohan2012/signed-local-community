1) fix float number precision stored in db 2) fixed sweep cut algorithm (pr scores normalized by degree)

algorithms/sweep_cut.py
@@ -6,16 +6,18 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from helpers import signed_conductance
+from helpers import signed_conductance, degree_array
 
 
-def get_community(g, scores):
-    order = np.argsort(scores)[::-1]
+def get_community(g, pr_scores):
+    degree_normalized_pr_scores = pr_scores / degree_array(g)
+    
+    order = np.argsort(degree_normalized_pr_scores)[::-1]
 
     sweep_positions = []
     sweep_scores = []
     for i in range(1, len(order)+1):
-        if scores[order[i-1]] == 0:
+        if degree_normalized_pr_scores[order[i-1]] == 0:
             break
         sweep_positions.append(i)
         s = signed_conductance(g, order[:i])

graph_generator/community_graph.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import networkx as nx
 import numpy as np
 import itertools as it
@@ -7,9 +8,9 @@ from tqdm import tqdm
 
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
-    sys.path.append(module_path
+    sys.path.append(module_path)
 
-from helpers import make_range
+from helpers import make_range, round_up
 
 
 def make_random_signed_graph(N, density=0.9, negatives_ratio=0.5):
@@ -96,8 +97,10 @@ def make_and_dump_batch(
             it.product(*params),
             total=np.prod(list(map(len, params)))
     ):
+        rounded_values = list(map(lambda v: round_up(v, 1), [p1, p2, p3, p4]))
+        p1, p2, p3, p4 = rounded_values
         outer_dir = "{}/id{:.1f}-in{:.1f}-ep{:.1f}-en{:.1f}".format(
-            output_dir, p1, p2, p3, p4
+            output_dir, *rounded_values
         )
         makedir_if_not_there(outer_dir)
 
@@ -109,6 +112,7 @@ def make_and_dump_batch(
                 external_edge_proba=p3,
                 external_neg_ratio=p4
             )
+
             g, comm = make(**params)
             output_path = '{}/{}.pkl'.format(outer_dir, i)
             

sql.py
@@ -1,4 +1,5 @@
 import psycopg2
+import json
 import numpy as np
 import pickle as pkl
 
@@ -20,7 +21,7 @@ class TableCreation:
     CREATE TABLE IF NOT EXISTS {schema}.{table_name}
     (
         graph_path TEXT,
-        graph_params BYTEA,
+        graph_params JSONB,
 
         method TEXT,
         query_node INTEGER,
@@ -86,7 +87,10 @@ def record_exists(cursor, table, record):
 def insert_record(cursor, schema, table, record):
     # convert complex type to pickle
     for k, v in record.items():
-        if isinstance(v, (list, tuple, dict, set, np.ndarray)):
+        if isinstance(v, dict):
+            record[k] = json.dumps(v)
+
+        if isinstance(v, (list, tuple, set, np.ndarray)):
             record[k] = pkl.dumps(v)
 
     cursor.execute(