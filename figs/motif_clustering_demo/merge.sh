d7a4ff5fd95a857c173859e2948507ccf7b30f40
Han Xiao <han.xiao@aalto.fi>
2019-02-19 03:09:07 +0000

    demo plots on motif clustering

---
+++ figs/motif_clustering_demo/merge.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+
+
+
+for d in $(ls -d *); do
+    if [ -d $d ]; then
+	pdftk $d/graph.pdf \
+	      $d/motif-graph.pdf \
+	      $d/sweep-profile-plot.pdf \
+	      $d/subgraph-selection.pdf \
+	      $d/subgraph-selected.pdf \
+	      $d/group-conductance-sweep.pdf \
+	      $d/partitioning.pdf \
+	      cat output \
+	      $d/all.pdf
+    fi
+done
+
