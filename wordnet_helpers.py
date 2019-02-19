bde76cf65fc2d4db0e0515aa27425f402edfc59e
Han Xiao <han.xiao@aalto.fi>
2019-02-19 03:55:46 +0000

    build wordnet graph

---
+++ wordnet_helpers.py
@@ -0,0 +1,41 @@
+"""
+main function: `build_graph`
+"""
+
+import networkx as nx
+from nltk.corpus import wordnet as wn
+from tqdm import tqdm
+
+
+def load_synonyms(lemma):
+    synonyms = set()
+    for syn in wn.synsets('good'):
+        for l in syn.lemmas():
+            synonyms.add(l.name())
+    return synonyms
+
+
+def load_antonyms(lemma):
+    antonyms = set()
+    for syn in wn.synsets('joy'):
+        for l in syn.lemmas():
+            for ant in l.antonyms():
+                antonyms.add(ant.name())
+    return antonyms
+
+
+def build_graph():
+    """build the graph"""
+    all_words = []
+    with open('graphs/wordnet/google-10000-english.txt', 'r') as f:
+        for l in f:
+            all_words.append(l.strip())
+
+    g = nx.Graph()
+    g.add_nodes_from(all_words)
+    for u in tqdm(all_words):
+        for v in load_synonyms(u):
+            g.add_edge(u, v, sign=1)
+        for v in load_antonyms(u):
+            g.add_edge(u, v, sign=-1)
+    nx.write_gpickle(g, 'graphs/wordnet.pkl')
