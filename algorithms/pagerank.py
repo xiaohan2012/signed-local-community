af8fa1249715d6b962eae0a3079ef4a370b4c5f7
Han Xiao <han.xiao@aalto.fi>
2019-02-20 06:59:32 +0000

    pagerank intializes using preference vect

--- algorithms/pagerank.py
+++ algorithms/pagerank.py
@@ -3,17 +3,81 @@ import networkx as nx
 from scipy.sparse import diags, csr_matrix
 
 
-def pr_score(g, q, alpha):
+def _pagerank_scipy(G, alpha=0.85, personalization=None,
+                    max_iter=100, tol=1.0e-6, weight='weight',
+                    dangling=None):
     """
-    wrapper of pagerank score calculation 
+    copied from networkx, but removes the `raise` part
+    """
+    
+    import scipy.sparse
+
+    N = len(G)
+    if N == 0:
+        return {}
+
+    nodelist = list(G)
+    M = nx.to_scipy_sparse_matrix(G, nodelist=nodelist, weight=weight,
+                                  dtype=float)
+    S = scipy.array(M.sum(axis=1)).flatten()
+    S[S != 0] = 1.0 / S[S != 0]
+    Q = scipy.sparse.spdiags(S.T, 0, *M.shape, format='csr')
+    M = Q * M
+
+    # Personalization vector
+    if personalization is None:
+        p = scipy.repeat(1.0 / N, N)
+    else:
+        p = scipy.array([personalization.get(n, 0) for n in nodelist], dtype=float)
+        p = p / p.sum()
+
+    # intial vector
+    x = p.copy()
+    
+    # Dangling nodes
+    if dangling is None:
+        dangling_weights = p
+    else:
+        # Convert the dangling dictionary into an array in nodelist order
+        dangling_weights = scipy.array([dangling.get(n, 0) for n in nodelist],
+                                       dtype=float)
+        dangling_weights /= dangling_weights.sum()
+    is_dangling = scipy.where(S == 0)[0]
+
+    # power iteration: make up to max_iter iterations
+    for _ in range(max_iter):
+        xlast = x
+        x = alpha * (x * M + sum(x[is_dangling]) * dangling_weights) + \
+            (1 - alpha) * p
+        # check convergence, l1 norm
+        err = scipy.absolute(x - xlast).sum()
+        if err < N * tol:
+            return dict(zip(nodelist, map(float, x)))
+    # return what I have, though not convergent
+    return dict(zip(nodelist, map(float, x)))
+
+
+def pr_score(g, q, alpha, max_iter=100):
+    """
+    wrapper of pagerank score calculation
     if graph is multi graph, use my own implementation of pagerank
     """
     if isinstance(g, (nx.MultiGraph, nx.MultiDiGraph)):
-        scores = pagerank_for_multi_graph(g, seeds=[q], alpha=alpha)
+        scores = pagerank_for_multi_graph(
+            g,
+            seeds=[q],
+            alpha=alpha,
+            max_iter=max_iter
+        )
     else:
         personalization = {n: 0.0 for n in g.nodes()}
         personalization[q] = 1.0
-        pr = nx.pagerank(g, alpha=1-alpha, personalization=personalization)
+        pr = _pagerank_scipy(
+            g,
+            alpha=1-alpha,
+            personalization=personalization,
+            max_iter=max_iter
+        )
         scores = np.zeros(g.number_of_nodes())
         for v, s in pr.items():
             scores[v] = s
