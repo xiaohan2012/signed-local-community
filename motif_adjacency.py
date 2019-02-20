a106523e4922f1e67a6da78848873ed6db2128ec
Han Xiao <han.xiao@aalto.fi>
2019-02-20 03:01:40 +0000

    fix bug on motif adjacency matrix construction

--- motif_adjacency.py
+++ motif_adjacency.py
@@ -1,12 +1,14 @@
 def motif_eef(A):
-    """motif: enemy - enemy - friend"""
+    """motif: enemy - enemy - friend
+    assumes A is array
+    """
     # path a ---(+)--- b ---(-)--- c
     C1 = (A @ A)
     C1[C1 > 0] = 0
 
     C2 = A.copy()
     C2[C2 > 0] = 0
-    pos_neg_C = C1 * C2
+    pos_neg_C = C1.multiply(C2)
 
     # path a ---(-)--- b ---(-)--- c
     neg_A = A.copy()
@@ -17,6 +19,6 @@ def motif_eef(A):
     C2 = A.copy()
     C2[C2 < 0] = 0
 
-    neg_neg_C = C1 * C2
+    neg_neg_C = C1.multiply(C2)
     W = pos_neg_C + neg_neg_C
     return W
