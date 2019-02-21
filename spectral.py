05ef7e186f7ca3866375ec00e979402ee654d48b
Han Xiao <han.xiao@aalto.fi>
2019-02-21 11:40:25 +0000

    signed spectral

---
+++ spectral.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import numpy as np
+from scipy.sparse.linalg import eigs
+
+from sklearn.cluster import KMeans
+
+from helpers import (
+    signed_laplacian,
+    signed_normalized_laplacian
+)
+
+
+def signed_spectral_clustering(g, k, normalize=False, weight='sign'):
+    A = nx.adj_matrix(g, weight=weight)
+    if normalize:
+        L = signed_normalized_laplacian(A)
+    else:
+        L = signed_laplacian(A)
+
+    eig_vals, eig_vects = eigs(L.asfptype(), k=k-1, which='SM')
+    eig_vects = eig_vects[:, np.argsort(eig_vals)]  # sort them by eigen values
+    eig_vects = np.real(eig_vects)
+    
+    embedding = eig_vects[:, :k-1]  # top-(k-1) eigen vectors
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(embedding)
+    return kmeans.labels_
+                
+
