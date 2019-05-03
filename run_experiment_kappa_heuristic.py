29d7bd8a06cf2c67558129bf985e81d85df49698
Han Xiao <han.xiao@aalto.fi>
2019-05-03 12:22:01 +0000

    experiment on kappa heursitic

--- run_experiment_kappa_heuristic.py
+++ run_experiment_kappa_heuristic.py
@@ -12,28 +12,6 @@ from helpers import noise_level, sample_seeds
 from exp_helpers import run_pipeline
 from data_helpers import make_polarized_graphs_fewer_parameters
 
-random.seed(12345)
-np.random.seed(12345)
-
-DEBUG = True
-
-if DEBUG:
-    n_graphs = 1
-    n_reps = 6
-    eta_list = np.linspace(0.01, 0.30, 2)
-else:
-    n_graphs = 10
-    n_reps = 64
-    eta_list = np.linspace(0.01, 0.30, 9)
-
-nc, nn = 10, 0
-k = 6
-seed_size = 3
-
-perf_list = []
-
-constant_kappa_list = [0.1, 0.5, 0.9]
-
 
 def argmin_kappa_by_seed_rank(g, seeds, kappa_min=0.1, kappa_max=0.95, num_kappa=18):
     mean_rank_list = []
@@ -59,24 +37,52 @@ def run_one_for_parallel(g, true_comms, true_groupings, seeds, target_comm, kapp
         res = run_pipeline(g, seeds, kappa, target_comm, true_comms, true_groupings, verbose=0)
         
         res['kappa'] = kappa
-        res['eta'] = eta
-        res['edge_noise_level'] = nl
+        res['seed_size'] = len(seeds[0])
         res['run_id'] = run_id
         return res
     except RuntimeError:
+        # cg error
         return False
 
 
-for eta in tqdm(eta_list):
-    for i in range(n_graphs):
-        g, true_comms, true_groupings = make_polarized_graphs_fewer_parameters(nc, nn, k, eta, verbose=0)
+random.seed(12345)
+np.random.seed(12345)
+
+DEBUG = False
+
+nc, nn = 10, 0
+k = 6
+seed_size = 3
+eta = 0.1
+
+
+if DEBUG:
+    n_graphs = 1
+    n_reps = 6
+    seed_size_list = np.arange(1, 2)
+else:
+    n_graphs = 10
+    n_reps = 64
+    seed_size_list = np.arange(1, nc)
+
+
+perf_list = []
+
+constant_kappa_list = [0.1, 0.5, 0.9]
+
+for i in range(n_graphs):
+    for seed_size in tqdm(seed_size_list):
+        g, true_comms, true_groupings = make_polarized_graphs_fewer_parameters(
+            nc, nn, k, eta, verbose=0
+        )
         nl = noise_level(g)
-        print("eta=", eta)
+        print("seed_size=", seed_size)
         print('|V|, |E|', g.number_of_nodes(), g.number_of_edges())
         print('noisy edge ratio: ', nl)
     
         seed_configs = [sample_seeds(true_comms, true_groupings, k=seed_size) for i in range(n_reps)]
-        
+
+        # run on constant kappas
         for kappa in constant_kappa_list:
             perf_list += Parallel(n_jobs=8)(
                 delayed(run_one_for_parallel)(
@@ -85,7 +91,8 @@ for eta in tqdm(eta_list):
                 )
                 for seeds, target_comm in seed_configs
             )
-        
+
+        # run on heuristic kappas
         heuristic_kappas = Parallel(n_jobs=8)(
             delayed(argmin_kappa_by_seed_rank)(
                 g, seeds=seeds
