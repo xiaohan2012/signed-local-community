caf2be857ed1c550d012ffd8e48f1a9e2b16db8e
Han Xiao <han.xiao@aalto.fi>
2019-05-07 19:33:53 +0000

    data conversion to matlab

---
+++ prepare_data_for_matlab.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import networkx as nx
+import scipy.io as sio
+from helpers import pos_adj, neg_adj
+
+graph_name = 'epinions'
+
+g = nx.read_gpickle('graphs/{}.pkl'.format(graph_name))
+
+
+adj = nx.adj_matrix(g, weight='sign')
+
+A, B = pos_adj(adj), neg_adj(adj)
+
+assert (A.data == 1).all()
+assert (B.data == 1).all()
+
+sio.savemat(
+    'data/{}.mat'.format(graph_name),
+    {'A': A, 'B': B},
+    format='4'  # your might need to change this according to your Matlab version
+)
