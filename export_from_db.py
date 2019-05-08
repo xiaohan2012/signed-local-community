caaf03ae228b38e411abf41cb78fb4facb1dbd7a
Han Xiao <han.xiao@aalto.fi>
2019-05-08 10:12:09 +0300

    fix data

--- export_from_db.py
+++ export_from_db.py
@@ -7,7 +7,9 @@ conn, cursor = init_db()
 
 graph = 'slashdot'
 cursor.execute("""
-    SELECT * FROM local_polarization.query_result
+    SELECT 
+        graph_path, query, kappa, k, C1, C2, best_beta, best_t, beta_array, ts, time_elapsed
+    FROM local_polarization.query_result
     WHERE graph_path LIKE '%%{}%%'
 """.format(graph)
 )
@@ -22,10 +24,10 @@ for r in tqdm(cursor.fetchall()):
     r[2] = float(r[2])
     r[4] = pkl.loads(r[4])
     r[5] = pkl.loads(r[5])
-    r[8] = pkl.loads(r[9])
+    r[8] = pkl.loads(r[8])
     r[9] = pkl.loads(r[9])
     rows.append(r)
 df = pd.DataFrame(rows, columns=cols)
-sub_df = df[['query', 'C1', 'C2', 'k', 'best_beta' ,'beta_array']]
+sub_df = df[['query', 'C1', 'C2', 'k', 'best_beta', 'beta_array']]
 sub_df = sub_df[sub_df['k'] == 200]
 sub_df.to_pickle('outputs/{}.pkl'.format(graph))
