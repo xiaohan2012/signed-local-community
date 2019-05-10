64500bc09c331b2fcfef6ffa9f5c26cfa5d9cf43
Han Xiao <han.xiao@aalto.fi>
2019-05-10 04:34:03 +0000

    stat_helpers.py and update notebook

---
+++ stat_helpers.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+def avg_pos_deg(pos_A, nodes):
+    """average positive degree"""
+    return pos_A[nodes, :][:, nodes].sum() / nodes.shape[0]
+
+
+def avg_neg_deg(neg_A, nodes):
+    """average negative degree"""
+    return neg_A[nodes, :][:, nodes].sum() / nodes.shape[0]
+
+
+def cohe(pos_A, C1, C2):
+    """coherence"""
+    n1, n2 = C1.shape[0], C2.shape[0]
+    cohe1 = pos_A[C1, :][:, C1].sum() / (n1 * (n1-1))
+    cohe2 = pos_A[C2, :][:, C2].sum() / (n2 * (n2-1))
+    return np.mean([cohe1, cohe2])
+
+
+def oppo(neg_A, C1, C2):
+    """opposition"""
+    return neg_A[C1, :][:, C2].sum() / C1.shape[0] / C2.shape[0]
+
+
+def agreement(pos_A, neg_A, C1, C2):
+    """edge agreement ratio"""
+    n_bad_pos = pos_A[C1, :][:, C2].sum() * 2
+    n_bad_neg = neg_A[C1, :][:, C1].sum() + neg_A[C2, :][:, C2].sum()
+    C = list(C1) + list(C2)
+    n_total = pos_A[C, :][:, C].sum() + neg_A[C, :][:, C].sum()
+    return 1 - (n_bad_pos + n_bad_neg) / n_total
+
+
+def populate_fields(df, pos_A, neg_A):
+    """use this!!!"""
+    cols = df.columns
+    existing_fields = ('C1', 'C2')
+    for f in existing_fields:
+        assert f in cols
+
+    df['posdeg1'] = df['C1'].apply(lambda v: avg_pos_deg(pos_A, v))
+    df['posdeg2'] = df['C2'].apply(lambda v: avg_pos_deg(pos_A, v))
+    df['negdeg1'] = df['C1'].apply(lambda v: avg_pos_deg(neg_A, v))
+    df['negdeg2'] = df['C2'].apply(lambda v: avg_pos_deg(neg_A, v))
+
+    df['max_posdeg'] = np.maximum(df['posdeg1'], df['posdeg2'])
+
+    df['coh'] = df[['C1', 'C2']].apply(lambda d: cohe(pos_A, d['C1'], d['C2']), axis=1)
+    df['opp'] = df[['C1', 'C2']].apply(lambda d: oppo(neg_A, d['C1'], d['C2']), axis=1)
+    df['ham'] = 2 * df['coh'] * df['opp'] / (df['coh'] + df['opp'])
+
+    df['agreement'] = df[['C1', 'C2']].apply(lambda d: agreement(pos_A, neg_A, d['C1'], d['C2']), axis=1)
+
+    assert (df['coh'] >= 0).all()
+    assert (df['coh'] <= 1).all()
+    assert (df['opp'] >= 0).all()
+    assert (df['opp'] <= 1).all()
+    assert (df['ham'] >= 0).all()
+    assert (df['ham'] <= 1).all()
+    assert (df['agreement'] >= 0).all()
+    assert (df['agreement'] <= 1).all()
+
+    return df
