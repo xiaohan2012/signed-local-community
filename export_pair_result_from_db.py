a561519fbd1c3aef491ecef85281b0a35691b960
Han Xiao <han.xiao@aalto.fi>
2019-05-12 17:20:42 +0300

    add best_beta

--- export_pair_result_from_db.py
+++ export_pair_result_from_db.py
@@ -8,20 +8,21 @@ conn, cursor = init_db()
 graph = 'bitcoin'
 cursor.execute("""
     SELECT 
-        seed1, seed2, k, C1, C2
+        seed1, seed2, k, C1, C2, best_beta
     FROM local_polarization.query_result_seed_pair
     WHERE graph_path LIKE '%%{}%%'
 """.format(graph)
 )
 
 cols = (
-    'seed1', 'seed2', 'k', 'C1', 'C2'
+    'seed1', 'seed2', 'k', 'C1', 'C2', 'best_beta'
 )
 rows = []
 for r in tqdm(cursor.fetchall()):
     r = list(r)
     r[3] = pkl.loads(r[3])
     r[4] = pkl.loads(r[4])
+    r[5] = float(r[5])
     rows.append(r)
 df = pd.DataFrame(rows, columns=cols)
 df.to_pickle('outputs/{}_seed_pair.pkl'.format(graph))
