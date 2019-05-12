9c28e70a472acb5d0c59eabdb77c414decd11f6a
Han Xiao <han.xiao@aalto.fi>
2019-05-12 09:32:56 +0000

    effect of seed size: also considers different kappas

--- run_experiment_effect_of_seed_size.py
+++ run_experiment_effect_of_seed_size.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 import networkx as nx
 
-from helpers import sample_seeds, noise_level, get_theoretical_kappa, sbr
+from helpers import sample_seeds, noise_level, sbr
 from data_helpers import make_polarized_graphs_fewer_parameters
 from exp_helpers import run_pipeline
 from joblib import Parallel, delayed
@@ -13,47 +13,24 @@ np.random.seed(12345)
 random.seed(12345)
 
 
-def run_one_for_parallel(g, true_comms, true_groupings, kappa_info, seed_size, nl, run_id):
+def run_one_for_parallel(g, true_comms, true_groupings, kappa, seed_size, nl, run_id):
     seeds, target_comm = sample_seeds(true_comms, true_groupings, k=seed_size)
-    try:
-        if kappa_info['use_suggested_kappa']:
-            kappa = get_theoretical_kappa(
-                true_comms[target_comm],
-                seeds,
-                nx.adj_matrix(g, weight='sign')
-            )
-        else:
-            kappa = kappa_info['kappa_default']
-
-        res = run_pipeline(g, seeds, kappa, target_comm, true_comms, true_groupings, verbose=0)
-        res['kappa'] = kappa
-        res['seed_size'] = seed_size
-        res['edge_noise_level'] = nl
-        res['run_id'] = run_id
-        res['ground_truth'] = true_groupings[target_comm]
-        res['ground_truth_beta'] = sbr(
-            nx.adj_matrix(g, weight='sign'),
-            true_groupings[target_comm][0], true_groupings[target_comm][1]
-        )
-        return res
-    except RuntimeError:
-        return dict(
-            kappa=None,
-            seed_size=None,
-            edge_noise_level=None,
-            run_id=None
-        )
-
-kappa_info = dict(
-    use_suggested_kappa=False,
-    kappa_default=0.8
-)
-
+    res = run_pipeline(g, seeds, kappa, target_comm, true_comms, true_groupings, verbose=0)
+    res['kappa'] = kappa
+    res['seed_size'] = seed_size
+    res['edge_noise_level'] = nl
+    res['run_id'] = run_id
+    res['ground_truth'] = true_groupings[target_comm]
+    res['ground_truth_beta'] = sbr(
+        nx.adj_matrix(g, weight='sign'),
+        true_groupings[target_comm][0], true_groupings[target_comm][1]
+    )
+    return res
 
 DEBUG = False
 
-nc, nn = 10, 0
-k = 6
+nc, nn = 20, 0
+k = 8
 eta = 0.05
 
 if DEBUG:
@@ -63,8 +40,9 @@ if DEBUG:
 else:
     n_graphs = 10
     n_reps = 60
-    seed_size_list = np.arange(1, nc)
+    seed_size_list = np.arange(1, 11)
 
+kappa_list = [0.1, 0.3, 0.5, 0.7, 0.9]
 
 if __name__ == "__main__":
     perf_list = []
@@ -77,17 +55,16 @@ if __name__ == "__main__":
             nl = noise_level(g)
             print("seed_size=", seed_size)
             print('noisy edge ratio: ', nl)
-            
-            perf_list += Parallel(n_jobs=8)(
-                delayed(run_one_for_parallel)(g, true_comms, true_groupings, kappa_info, seed_size, nl, i)
-                for i in range(n_reps)
-            )
+
+            for kappa in kappa_list:
+                print('kappa =', kappa)
+                perf_list += Parallel(n_jobs=8)(
+                    delayed(run_one_for_parallel)(g, true_comms, true_groupings, kappa, seed_size, nl, i)
+                    for i in range(n_reps)
+                )
 
     perf_df = pd.DataFrame.from_records(perf_list)
 
-    if kappa_info['use_suggested_kappa']:
-        output_path = 'outputs/effect_of_seed_size_suggested_kappa.csv'
-    else:
-        output_path = 'outputs/effect_of_seed_size.csv'
+    output_path = 'outputs/effect_of_seed_size.csv'
     print('saving to', output_path)
     perf_df.to_csv(output_path)
