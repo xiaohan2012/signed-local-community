f9ddd05a7edc3651837b1545cd24c3f263096185
Han Xiao <han.xiao@aalto.fi>
2019-05-22 20:27:46 +0000

    pack code

---
+++ pack_code.sh
@@ -0,0 +1,14 @@
+#! /bin/zsh
+
+zip code.zip \
+    *.py \
+    graphs/*.pkl \
+    *.md \
+    *.yml \
+    sh/*.sh \
+    intro-plot.ipynb \
+    graph_stat_table.ipynb \
+    experiment_on_synthetic_graphs.ipynb \
+    FOCG-vs-PolarSeeds.ipynb \
+    case-study-overlapping-community.ipynb \
+    case-study-distrust-radiation.ipynb
