fix lambda issue and added precision arguments to pipeline

run_experiment_effect_of_seed_size.py
@@ -16,14 +16,17 @@ random.seed(12345)
 def run_one_for_parallel(g, true_comms, true_groupings, kappa, seed_size, nl, run_id):
     seeds, target_comm = sample_seeds(true_comms, true_groupings, k=seed_size)
     res = run_pipeline(
-        g, seeds, kappa, target_comm, true_comms, true_groupings, verbose=0, return_details=True
+        g, seeds, kappa, target_comm, true_comms, true_groupings,
+        max_iter=99999,
+        tol=1e-3,
+        verbose=0, return_details=True
     )
     res['kappa'] = kappa
     res['seed_size'] = seed_size
     res['edge_noise_level'] = nl
     res['run_id'] = run_id
     res['alpha'] = res['runtime_details']['alpha']
-    res['lambda1'] = res['runtime_details']['lambda1']
+    res['lambda1'] = float(np.real(res['runtime_details']['lambda1']))
     del res['runtime_details']
     res['ground_truth'] = true_groupings[target_comm]
     res['ground_truth_beta'] = sbr(
@@ -32,7 +35,7 @@ def run_one_for_parallel(g, true_comms, true_groupings, kappa, seed_size, nl, ru
     )
     return res
 
-DEBUG = True
+DEBUG = False
 
 nc, nn = 20, 0
 k = 8